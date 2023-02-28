a609ef9a86ddec5cb41bc66f8885f4c9a6ad0b3a
Eduard Caziuc <caziuc.eduard@yahoo.com>
2023-02-28 21:49:54 +0200

    Fix indentation and replace 0 with .zero.

--- Line Chart.swiftpm/ContentView.swift
+++ Line Chart.swiftpm/ContentView.swift
@@ -8,23 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-   @State var chartData: [Double] = [65054, 61000, 62420, 56030, 57089, 65021, 59060, 67000, 56006, 65070, 54000, 42000, 88000, 49000, 42000, 61000, 67000, 54000, 47000, 42000, 71000, 56000, 81000, 71000, 40000, 49000, 42000, 58000, 66000, 62000, 77000, 52000, 42000, 49000, 58000, 61000, 68000, 43000, 49000, 69000, 81000]
-   @State var presentGraphSheet: Bool = false
+	@State var chartData: [Double] = [65054, 61000, 62420, 56030, 57089, 65021, 59060, 67000, 56006, 65070, 54000, 42000, 88000, 49000, 42000, 61000, 67000, 54000, 47000, 42000, 71000, 56000, 81000, 71000, 40000, 49000, 42000, 58000, 66000, 62000, 77000, 52000, 42000, 49000, 58000, 61000, 68000, 43000, 49000, 69000, 81000]
+	@State var presentGraphSheet: Bool = false
 	@State var pointMarkSize: Double = 0
-   
-   var body: some View {
-      GeometryReader { geometry in
+	
+	var body: some View {
+		GeometryReader { geometry in
 			VStack(spacing: UIDevice.current.orientation.isLandscape ? 4 : 20) {
-            lineGraph
-               .frame(width: geometry.size.width * 0.9, height: geometry.size.height * (UIDevice.current.orientation.isLandscape ? 0.6 : 0.2))
-            
-            Button {
-               chartData.append(chartData.randomElement() ?? 20000)
-            } label: {
-               Text("Add Value")
-            }
-            .buttonStyle(.borderedProminent)
-
+				lineGraph
+					.frame(width: geometry.size.width * 0.9, height: geometry.size.height * (UIDevice.current.orientation.isLandscape ? 0.6 : 0.2))
+				
+				Button {
+					chartData.append(chartData.randomElement() ?? 20000)
+				} label: {
+					Text("Add Value")
+				}
+				.buttonStyle(.borderedProminent)
+				
 				VStack {
 					Slider(value: $pointMarkSize, in: 0...10)
 					if UIDevice.current.orientation.isPortrait {
@@ -33,81 +33,81 @@ struct ContentView: View {
 				}
 				.padding(.horizontal)
 			}
-         .frame(maxWidth: .infinity, maxHeight: .infinity)
-         
-         .onTapGesture {
-            presentGraphSheet = true
-         }
-      }      
-   }
-   
-   @ViewBuilder
-   var lineGraph: some View {
-      LineChart(data: LineChart.createPointMarks(with: chartData), pointMarkSize: pointMarkSize)
-   }
+			.frame(maxWidth: .infinity, maxHeight: .infinity)
+			
+			.onTapGesture {
+				presentGraphSheet = true
+			}
+		}      
+	}
+	
+	@ViewBuilder
+	var lineGraph: some View {
+		LineChart(data: LineChart.createPointMarks(with: chartData), pointMarkSize: pointMarkSize)
+	}
 }
 
 struct ContentView2: View {
-   var graphData: [CGPoint] = []
-   
-   init() {
-      var expensePointMarks: [CGPoint] = []
-      for expense in Expense.allExpenses {
-         expensePointMarks.append(CGPoint(x: Double(expense.xValue), y: expense.expense))
-      }
-      graphData = expensePointMarks
-   }
-   
-   var body: some View {
-      GeometryReader { geometry in
-         Group {
-            LineChart(data: graphData)
-               .frame(width: geometry.size.width * 0.9, height: geometry.size.height * (UIDevice.current.orientation.isLandscape ? 0.6 : 0.2))
-         }
-         .frame(maxWidth: .infinity, maxHeight: .infinity)
-      }
-   }
+	var graphData: [CGPoint] = []
+	
+	init() {
+		var expensePointMarks: [CGPoint] = []
+		for expense in Expense.allExpenses {
+			expensePointMarks.append(CGPoint(x: Double(expense.xValue), y: expense.expense))
+		}
+		graphData = expensePointMarks
+	}
+	
+	var body: some View {
+		GeometryReader { geometry in
+			Group {
+				LineChart(data: graphData)
+					.frame(width: geometry.size.width * 0.9, height: geometry.size.height * (UIDevice.current.orientation.isLandscape ? 0.6 : 0.2))
+			}
+			.frame(maxWidth: .infinity, maxHeight: .infinity)
+		}
+	}
 }
 
 struct Expense {
-   enum Month: Int {
-      case jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec
-   }
-   
-   let month: Month
-   let expense: Double
-   
-   var quarter: Int {
-      switch month {
-      case .jan, .feb, .mar:
-         return 1
-      case .apr, .may, .jun:
-         return 2
-      case .jul, .aug, .sep:
-         return 3
-      default:
-         return 4
-         
-      }
-   }
-   
-   var xValue: Double {Double(month.rawValue) }
-   
-   static var allExpenses: [Expense] {
-      [
-         Expense(month: .jan, expense: 3500),
-         Expense(month: .feb, expense: 5000),
-         Expense(month: .mar, expense: 5500),
-         Expense(month: .apr, expense: 5100),
-         Expense(month: .may, expense: 4200),
-         Expense(month: .jun, expense: 7500),
-         Expense(month: .jul, expense: 1250),
-         Expense(month: .aug, expense: 9000),
-         Expense(month: .sep, expense: 5750),
-         Expense(month: .oct, expense: 6000),
-         Expense(month: .nov, expense: 2100),
-         Expense(month: .dec, expense: 6000)
-         
-      ]
-   }
+	enum Month: Int {
+		case jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec
+	}
+	
+	let month: Month
+	let expense: Double
+	
+	var quarter: Int {
+		switch month {
+		case .jan, .feb, .mar:
+			return 1
+		case .apr, .may, .jun:
+			return 2
+		case .jul, .aug, .sep:
+			return 3
+		default:
+			return 4
+			
+		}
+	}
+	
+	var xValue: Double {Double(month.rawValue) }
+	
+	static var allExpenses: [Expense] {
+		[
+			Expense(month: .jan, expense: 3500),
+			Expense(month: .feb, expense: 5000),
+			Expense(month: .mar, expense: 5500),
+			Expense(month: .apr, expense: 5100),
+			Expense(month: .may, expense: 4200),
+			Expense(month: .jun, expense: 7500),
+			Expense(month: .jul, expense: 1250),
+			Expense(month: .aug, expense: 9000),
+			Expense(month: .sep, expense: 5750),
+			Expense(month: .oct, expense: 6000),
+			Expense(month: .nov, expense: 2100),
+			Expense(month: .dec, expense: 6000)
+			
+		]
+	}
 }
--- Line Chart.swiftpm/LineChart.swift
+++ Line Chart.swiftpm/LineChart.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct LineChart: View {
 	// MARK: - Public Properties
-
+	
 	var data: [CGPoint]
 	var enableIndicator: Bool
 	var lineMarkWidth: Double
@@ -17,9 +17,9 @@ struct LineChart: View {
 	var numberOfGridlines: Int
 	var axisBaseValue: Double?
 	var dashedGridlines: Bool
-
+	
 	// MARK: - Private Properties
-
+	
 	@State private var isDragging: Bool = false
 	@State private var currentYLabel: Double = .zero
 	@State private var pointMarkReached: Double = .zero
@@ -36,10 +36,10 @@ struct LineChart: View {
 	private var yAxis: Double = .zero
 	private var yLabels: [Double] { data.map { $0.y } }
 	private var xLabels: [Double] { data.map { $0.x } }
-
+	
 	// MARK: - Initialization
-
-	init(data: [CGPoint], enablePositionIndicator: Bool = true, lineMarkWidth: Double = 3, pointMarkSize: Double = 0, numberOfGridlines: Int = 4, axisBaseValue: Double? = .zero, dashedGridlines: Bool = true) {
+	
+	init(data: [CGPoint], enablePositionIndicator: Bool = true, lineMarkWidth: Double = 3, pointMarkSize: Double = .zero, numberOfGridlines: Int = 4, axisBaseValue: Double? = .zero, dashedGridlines: Bool = true) {
 		self.data = data
 		self.enableIndicator = enablePositionIndicator
 		self.lineMarkWidth = lineMarkWidth
@@ -49,44 +49,44 @@ struct LineChart: View {
 		self.dashedGridlines = dashedGridlines
 		setupChart()
 	}
-
+	
 	var body: some View {
 		GeometryReader { geometry in
 			HStack(spacing: 4) {
-
+				
 				VStack(alignment: .trailing, spacing: yLegendOffset) {
 					ForEach(0...numberOfGridlines, id: \.self) { gridNumber in
-						Text("$\(yLegendLabel(for: gridNumber), specifier: "%.0f")").offset(x: 0, y: yLegendPosition(for: gridNumber, geometrySize: geometry.size))
+						Text("$\(yLegendLabel(for: gridNumber), specifier: "%.0f")").offset(x: .zero, y: yLegendPosition(for: gridNumber, geometrySize: geometry.size))
 							.foregroundColor(.gray)
 							.font(.caption2)
 					}
 				}
-
+				
 				GeometryReader { geometry in
 					ZStack {
 						LinearGradient(gradient: Gradient(colors: [lineColor.opacity(0.6), lineColor.opacity(0.3), lineColor.opacity(0.1)]), startPoint: .top, endPoint: .bottom)
 							.clipShape(lineMark(in: geometry, closedPath: true))
-							.opacity(animationProgress == 1 ? 1 : 0)
-
+							.opacity(animationProgress == 1 ? 1 : .zero)
+						
 						lineMark(in: geometry)
-							.trim(from: 0, to: animationProgress)
+							.trim(from: .zero, to: animationProgress)
 							.stroke(lineColor, style: StrokeStyle(lineWidth: lineMarkWidth, lineCap: .round, lineJoin: .round))
 							.overlay {
-								if pointMarkSize > 0 {
+								if pointMarkSize > .zero {
 									pointMarks(in: geometry)
 										.fill(lineColor)
-										.opacity(animationProgress == 1 ? 1 : 0)
+										.opacity(animationProgress == 1 ? 1 : .zero)
 								}
 							}
-
+						
 						if enableIndicator { positionIndicator(in: geometry) }
 					}
 				}
 				.background {
 					ForEach((0...numberOfGridlines), id: \.self) { gridNumber in
 						gridlines(at: yLegendLabel(for: gridNumber), in: geometry.size)
-							.trim(from: 0, to: animationProgress)
-							.stroke(.gray, style: dashedGridlines ? StrokeStyle(lineWidth: gridNumber == numberOfGridlines ? 2 : 0.6, lineCap: .square, dash: [2, gridNumber == numberOfGridlines ? 0 : 2]) : StrokeStyle(lineWidth: gridNumber == 0 ? 2 : 0.4, lineCap: .square)
+							.trim(from: .zero, to: animationProgress)
+							.stroke(.gray, style: dashedGridlines ? StrokeStyle(lineWidth: gridNumber == numberOfGridlines ? 2 : 0.6, lineCap: .square, dash: [2, gridNumber == numberOfGridlines ? .zero : 2]) : StrokeStyle(lineWidth: gridNumber == .zero ? 2 : 0.4, lineCap: .square)
 							)
 							.clipped()
 					}
@@ -94,13 +94,13 @@ struct LineChart: View {
 			}
 			.background {
 				chartAxis(in: geometry.size)
-					.trim(from: 0, to: animationProgress)
+					.trim(from:  .zero, to: animationProgress)
 					.stroke(.gray, lineWidth: 1)
 			}
 		}
 		.onAppear { withAnimation(.easeIn(duration: 2).delay(0.3)) { animationProgress = 1 } }
 	}
-
+	
 	static func createPointMarks(with array: [Double]) -> [CGPoint] {
 		var data: [CGPoint] = []
 		for (index, value) in array.enumerated() {
@@ -118,19 +118,19 @@ private extension LineChart {
 			for (index, value) in data.enumerated() {
 				let xPosition = CGFloat((value.x - minX) / xAxis) * geometry.size.width
 				let yPosition = (1 - CGFloat((value.y - minY) / yAxis)) * geometry.size.height
-				if index == 0 { path.move(to: CGPoint(x: xPosition, y: yPosition)) }
+				if index == .zero { path.move(to: CGPoint(x: xPosition, y: yPosition)) }
 				//else { path.move(to: CGPoint(x: xPosition, y: geometry.size.height)) } // For bar graph style.
 				path.addLine(to: CGPoint(x: xPosition, y: yPosition))
 			}
-
+			
 			if closedPath {
 				path.addLine(to: CGPoint(x: geometry.size.width, y: geometry.size.height))
-				path.addLine(to: CGPoint(x: 0, y: geometry.size.height))
+				path.addLine(to: CGPoint(x: .zero, y: geometry.size.height))
 				path.closeSubpath()
 			}
 		}
 	}
-
+	
 	func pointMarks(in geometry: GeometryProxy) -> Path {
 		Path { path in
 			for (_, value) in data.enumerated() {
@@ -141,12 +141,12 @@ private extension LineChart {
 			}
 		}
 	}
-
+	
 	@ViewBuilder
 	func positionIndicator(in geometry: GeometryProxy) -> some View {
 		let boxHeight: CGFloat = 60
-		let boxOffset = max(0, min(geometry.size.width - boxHeight, dragGestureXLocation - boxHeight / 2))
-
+		let boxOffset = max(.zero, min(geometry.size.width - boxHeight, dragGestureXLocation - boxHeight / 2))
+		
 		Group {
 			GroupBox {
 				Text("$\(currentYLabel, specifier: "%.0f")")
@@ -154,27 +154,27 @@ private extension LineChart {
 			}
 			.frame(height: boxHeight)
 			.position(x:(boxOffset) + boxHeight / 2, y: -(boxHeight / 2))
-
+			
 			Rectangle()
 				.fill(.indigo)
 				.frame(width: 2, height: geometry.size.height)
 				.position(x: dragGestureXLocation, y: geometry.size.height / 2)
-
+			
 			Circle()
 				.fill(Color.white)
 				.shadow(radius: 1)
 				.frame(width: 10, height: 10)
 				.position(x: dragGestureXLocation, y: getYPosition(forXPosition: dragGestureXLocation, in: geometry.size))
 		}
-		.opacity(isDragging ? 1 : 0)
+		.opacity(isDragging ? 1 : .zero)
 		.contentShape(Rectangle())
-
+		
 		.gesture(
 			DragGesture(minimumDistance: .zero)
 				.onChanged { value in
 					isDragging = true
-
-					if((value.location.x < geometry.size.width) && (value.location.x > 0)) {
+					
+					if((value.location.x < geometry.size.width) && (value.location.x > .zero)) {
 						dragGestureXLocation = value.location.x
 					}
 				}
@@ -186,14 +186,14 @@ private extension LineChart {
 			selectionFeedbackGenerator.selectionChanged()
 		}
 	}
-
+	
 	func gridlines(at height: CGFloat, in geometrySize: CGSize) -> Path {
 		Path { path in
 			path.move(to: CGPoint(x: .zero, y: (height - minY) * stepHeight(geometrySize: geometrySize)))
 			path.addLine(to: CGPoint(x: geometrySize.width, y: (height - minY) * stepHeight(geometrySize: geometrySize)))
 		}
 	}
-
+	
 	func chartAxis(in geometrySize: CGSize) -> Path {
 		Path { path in
 			path.move(to: CGPoint(x: geometrySize.width, y: .zero))
@@ -206,21 +206,21 @@ private extension LineChart {
 
 private extension LineChart {
 	// Position Indicator Methods
-
+	
 	mutating func setupChart() {
 		if !data.isEmpty {
 			// If axisBaseValue is not set, the minY value will be the lowest value in the data array.
 			minY = axisBaseValue == nil ? yLabels.min() ?? .zero : axisBaseValue ?? .zero
-			maxY = yLabels.max() != 0 ? yLabels.max() ?? 0 : 1
-			minX = xLabels.min() ?? 0
-			maxX = xLabels.max() ?? 0
-
+			maxY = yLabels.max() != .zero ? yLabels.max() ?? .zero : 1
+			minX = xLabels.min() ?? .zero
+			maxX = xLabels.max() ?? .zero
+			
 			// Sets the color of the line mark to green if the data is in an upwards trend or to red if it's in a downwards trend.
 			if data.count > 1 {
 				let deltaY = data[data.count - 1].y - data[data.count - 2].y
-				if (deltaY >= 0) {
+				if (deltaY >= .zero) {
 					lineColor = Color.green
-				} else if (deltaY < 0) {
+				} else if (deltaY < .zero) {
 					lineColor = .red
 				}
 			} else {
@@ -230,23 +230,23 @@ private extension LineChart {
 			yAxis = maxY - minY
 		}
 	}
-
+	
 	func yLabel(forXPosition xPosition: Double, in geometrySize: CGSize) -> Double {
 		let xLabel = xLabel(forXPosition: xPosition, in: geometrySize)
-		var result = Double(0)
-
+		var result: Double = .zero
+		
 		for index in 1..<data.count {
 			let lowerBound = data[index - 1].x
 			let upperBound = data[index].x
-
+			
 			if ((xLabel <= upperBound) && (xLabel >= lowerBound)) {
 				let interpolationFraction = (upperBound - lowerBound) / (xLabel - lowerBound)
 				result = data[index - 1].y + (data[index].y - data[index - 1].y) / interpolationFraction
-
+				
 				//Set currentYLabel value for position indicator.
 				DispatchQueue.main.async {
-					if round(xLabel) == xLabels[0] {
-						currentYLabel = yLabels[0]
+					if round(xLabel) == xLabels[.zero] {
+						currentYLabel = yLabels[.zero]
 					} else if round(xLabel) == round(data[Int(upperBound)].x) {
 						currentYLabel = data[index].y
 					}
@@ -256,18 +256,18 @@ private extension LineChart {
 		}
 		return result
 	}
-
+	
 	func xLabel(forXPosition xPosition: Double, in geometrySize: CGSize) -> Double {
 		return ((xAxis * xPosition) / geometrySize.width) + minX
 	}
-
+	
 	func getYPosition(forXPosition xPosition: Double, in geometrySize: CGSize) -> Double {
 		let yLabel = yLabel(forXPosition: xPosition, in: geometrySize)
 		return (1 - CGFloat((yLabel - minY) / yAxis)) * geometrySize.height
 	}
-
+	
 	// Legend Methods
-
+	
 	func yLegendLabels() -> [Double]? {
 		let step = Double(maxY - minY) / Double(numberOfGridlines)
 		var yLegendLabels: [Double] = []
@@ -276,17 +276,17 @@ private extension LineChart {
 		}
 		return yLegendLabels
 	}
-
+	
 	func yLegendLabel(for gridNumber: Int) -> CGFloat{
-		guard let legendLabels = yLegendLabels() else { return 0 }
+		guard let legendLabels = yLegendLabels() else { return .zero }
 		return CGFloat(legendLabels[gridNumber])
 	}
-
+	
 	func yLegendPosition(for gridNumber: Int, geometrySize: CGSize) -> CGFloat {
-		guard let legend = yLegendLabels() else { return 0 }
+		guard let legend = yLegendLabels() else { return .zero }
 		return (geometrySize.height - ((CGFloat(legend[gridNumber]) - minY) * stepHeight(geometrySize: geometrySize))) - (geometrySize.height / 2)
 	}
-
+	
 	func stepHeight(geometrySize: CGSize) -> CGFloat {
 		return geometrySize.height / yAxis
 	}
